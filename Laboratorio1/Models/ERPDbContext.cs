5392a601a11e85330650550a11e7fccf37e1041a
Jorge Garcia <101574832+22Jgarcia@users.noreply.github.com>
2025-04-02 14:14:19 -0600

    primer commit

---
+++ Laboratorio1/Models/ERPDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Laboratorio1.Models
+{
+    public class ERPDbContext : IdentityDbContext<IdentityUser>
+    {
+        public ERPDbContext(DbContextOptions<ERPDbContext> options) : base(options)
+        {
+
+        }
+
+        
+    }
+}
