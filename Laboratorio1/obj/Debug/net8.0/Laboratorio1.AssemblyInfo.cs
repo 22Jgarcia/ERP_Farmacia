8537a5fbb974b16007c78517844a13646967d96c
Jorge Garcia <101574832+22Jgarcia@users.noreply.github.com>
2025-04-02 20:52:51 -0600

    cachee

--- Laboratorio1/obj/Debug/net8.0/Laboratorio1.AssemblyInfo.cs
+++ Laboratorio1/obj/Debug/net8.0/Laboratorio1.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Laboratorio1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5392a601a11e85330650550a11e7fccf37e1041a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Laboratorio1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Laboratorio1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
