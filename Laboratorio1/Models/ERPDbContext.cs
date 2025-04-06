6b770f457b96d40efe9124e6a69531f0232c34d8
Jorge Garcia <101574832+22Jgarcia@users.noreply.github.com>
2025-04-05 22:02:04 -0600

    actualizacion de vista create y del ERPDbContext
    en el ERPbContext se le agrego otra funcion para que no de comflictos las llaves foraneas con la tabla de inventaio y movimientos Inventario,  y la vista create para solo seleccionar compra y venta

--- Laboratorio1/Models/ERPDbContext.cs
+++ Laboratorio1/Models/ERPDbContext.cs
@@ -12,11 +12,34 @@ namespace Laboratorio1.Models
 
         }
         public DbSet<CategoriaProducto> CategoriaProductos { get; set; }
-        public DbSet<Proveedor> Proveedores { get; set; }
-        public DbSet<Producto> Productos { get; set; }
+		public DbSet<Proveedor> Proveedores { get; set; }
+		public DbSet<Producto> Productos { get; set; }
 		public DbSet<Lote> Lotes { get; set; }
 		public DbSet<Movimientos_Inventario> MovimientoInventarios { get; set; }
+		public DbSet<Inventario> Inventario { get; set; } = default!;
 
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			base.OnModelCreating(modelBuilder);
 
+			// Evitar múltiples cascadas desde tbl_productos
+			modelBuilder.Entity<Inventario>()
+			.HasOne(i => i.Producto)
+			.WithMany()
+			.HasForeignKey(i => i.ProductoId)
+			.OnDelete(DeleteBehavior.Restrict); // o .NoAction
+
+			modelBuilder.Entity<Movimientos_Inventario>()
+			.HasOne(m => m.Producto)
+			.WithMany()
+			.HasForeignKey(m => m.ProductoId)
+			.OnDelete(DeleteBehavior.Restrict);
+
+			modelBuilder.Entity<Lote>()
+			.HasOne(l => l.Producto)
+			.WithMany()
+			.HasForeignKey(l => l.ProductoId)
+			.OnDelete(DeleteBehavior.Restrict);
+		}
 	}
 }
